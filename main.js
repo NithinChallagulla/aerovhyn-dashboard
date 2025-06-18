dc97a8cc240459301badc65d06d6cc79af10cae4
Nithin Challagulla <115912319+NithinChallagulla@users.noreply.github.com>
2025-06-18 23:04:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-import './style.css';
-import { firestore } from './firebase.js';
+// Firebase via CDN
+const firebaseConfig = {
+  apiKey: "AIzaSyDooJlQCfk104yS4XzOj1bgpVEoOB8rTnQ",
+  authDomain: "webrtc-demo-33437.firebaseapp.com",
+  projectId: "webrtc-demo-33437",
+  storageBucket: "webrtc-demo-33437.appspot.com",
+  messagingSenderId: "99021841417",
+  appId: "1:99021841417:web:510b0562a0d20c437ec4d8",
+  measurementId: "G-H2KFF0EHS0"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+
+// RTC setup remains same
 const servers = {
-  iceServers: [
-    { urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] }
-  ],
+  iceServers: [{ urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] }],
   iceCandidatePoolSize: 10,
 };
 
@@ -12,6 +23,7 @@ const pc = new RTCPeerConnection(servers);
 let localStream = null;
 let remoteStream = null;
 
+// HTML elements
 const webcamButton = document.getElementById('webcamButton');
 const webcamVideo = document.getElementById('webcamVideo');
 const callButton = document.getElementById('callButton');
@@ -20,12 +32,16 @@ const answerButton = document.getElementById('answerButton');
 const remoteVideo = document.getElementById('remoteVideo');
 const hangupButton = document.getElementById('hangupButton');
 
+// Webcam start
 webcamButton.onclick = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
   remoteStream = new MediaStream();
 
   localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
-  pc.ontrack = event => event.streams[0].getTracks().forEach(track => remoteStream.addTrack(track));
+
+  pc.ontrack = event => {
+    event.streams[0].getTracks().forEach(track => remoteStream.addTrack(track));
+  };
 
   webcamVideo.srcObject = localStream;
   remoteVideo.srcObject = remoteStream;
@@ -35,16 +51,23 @@ webcamButton.onclick = async () => {
   webcamButton.disabled = true;
 };
 
+// Create call
 callButton.onclick = async () => {
   const callDoc = firestore.collection('calls').doc();
   const offerCandidates = callDoc.collection('offerCandidates');
   const answerCandidates = callDoc.collection('answerCandidates');
+
   callInput.value = callDoc.id;
 
-  pc.onicecandidate = e => e.candidate && offerCandidates.add(e.candidate.toJSON());
+  pc.onicecandidate = event => {
+    if (event.candidate) {
+      offerCandidates.add(event.candidate.toJSON());
+    }
+  };
 
   const offerDescription = await pc.createOffer();
   await pc.setLocalDescription(offerDescription);
+
   await callDoc.set({ offer: { type: offerDescription.type, sdp: offerDescription.sdp } });
 
   callDoc.onSnapshot(snapshot => {
@@ -65,18 +88,25 @@ callButton.onclick = async () => {
   hangupButton.disabled = false;
 };
 
+// Answer call
 answerButton.onclick = async () => {
   const callId = callInput.value;
   const callDoc = firestore.collection('calls').doc(callId);
   const answerCandidates = callDoc.collection('answerCandidates');
   const offerCandidates = callDoc.collection('offerCandidates');
 
-  pc.onicecandidate = e => e.candidate && answerCandidates.add(e.candidate.toJSON());
+  pc.onicecandidate = event => {
+    if (event.candidate) {
+      answerCandidates.add(event.candidate.toJSON());
+    }
+  };
 
   const callData = (await callDoc.get()).data();
   await pc.setRemoteDescription(new RTCSessionDescription(callData.offer));
+
   const answerDescription = await pc.createAnswer();
   await pc.setLocalDescription(answerDescription);
+
   await callDoc.update({ answer: { type: answerDescription.type, sdp: answerDescription.sdp } });
 
   offerCandidates.onSnapshot(snapshot => {
@@ -86,11 +116,4 @@ answerButton.onclick = async () => {
       }
     });
   });
-
-  hangupButton.disabled = false;
-};
-
-hangupButton.onclick = () => {
-  pc.close();
-  window.location.reload();
 };
